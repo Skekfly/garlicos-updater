0de7de3f96ff5c5c7511e840b09a27fec930b743
flolep <mail@flolep.com>
2023-03-13 23:54:25 +0100

    feat: adb push + shell cp are working

--- garlicos-updater.sh
+++ garlicos-updater.sh
@@ -1,46 +1,36 @@
 #!/bin/bash
 
-echo "Start"
+echo "# Start"
 
-source garlicos-updater.cfg
+source ./garlicos-updater.cfg
+workingDir=/tmp/garlicOS/
 
 mkdir -p "$workingDir" || exit
 cd "$workingDir" || exit
 
-echo "Downloading update package"
+echo "# Downloading update package"
 wget -O garlicOS.7z "$garlicOsDownloadPath" || exit
 7z x garlicOS.7z || exit
 
-echo "Updating misc"
-adb -s "$adbDeviceId" push misc/* /misc/ || exit
+echo "# Updating misc"
+adb -s "$adbDeviceId" shell cp -Rv misc/* /misc/ || exit
 
-echo "Updating roms (int)"
+echo "# Updating roms (int)"
 adb -s "$adbDeviceId" push roms/Roms/* /mnt/mmc/ || exit
+echo "# Updating roms (ext)"
+adb -s "$adbDeviceId" push roms/Roms/* /mnt/SDCARD/ || exit
 
-if [ -d "/mnt/SDCARD/" ]; then
-  echo "Updating roms (ext)"
-  adb -s "$adbDeviceId" push roms/Roms/* /mnt/SDCARD/ || exit
-fi
-
-echo "Updating bios (int)"
+echo "# Updating bios (int)"
 adb -s "$adbDeviceId" push roms/BIOS /mnt/mmc/ || exit
+echo "# Updating bios (ext)"
+adb -s "$adbDeviceId" push roms/BIOS /mnt/SDCARD/ || exit
 
-if [ -d "/mnt/SDCARD/" ]; then
-  echo "Updating bios (ext)"
-  adb -s "$adbDeviceId" push roms/BIOS /mnt/SDCARD/ || exit
-fi
+echo "# Updating cfw (int)"
+adb -s "$adbDeviceId" push roms/CFW /mnt/mmc/ || exit
+echo "# Updating cfw (ext)"
+adb -s "$adbDeviceId" push roms/CFW /mnt/SDCARD/ || exit
 
-echo "Updating cfw (int)"
-adb -s "$adbDeviceId" push roms/CFW /mnt/mmc/* || exit
+echo "# Saving saves (int)"
+adb -s "$adbDeviceId" pull /mnt/SDCARD/Saves "$extSaveDir" || exit
 
-if [ -d "/mnt/SDCARD/" ]; then
-  echo "Updating cfw (ext)"
-  adb -s "$adbDeviceId" push roms/CFW /mnt/SDCARD/ || exit
-fi
-
-if [ -d "/mnt/SDCARD/" ]; then
-  echo "Saving saves (int)"
-  adb -s "$adbDeviceId" pull /mnt/SDCARD/Saves "$extSaveDir" || exit
-fi
-
-echo "End"
+echo "# End"
